baa7d70b0d2b20aa4d6b82690f7fc3096ec9d512
Max <visze@users.noreply.github.com>
2020-04-06 21:01:56 +0200

    prepare minor release

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
 #    test_suite='tests',
 #    tests_require=test_requirements,
     url='https://github.com/sroener/negative_training_sampler',
-    version='0.1.0-dev',
+    version='0.2.0',
     zip_safe=False,
 )
